d7dfdfd8d02e1236e4ccb719d1577d4fef566834
Tolpachino <144340436+Tolpachino@users.noreply.github.com>
2023-09-19 20:09:59 +1000

    Update Exercise2.java

--- src/Homework/Exercise2.java
+++ src/Homework/Exercise2.java
@@ -1,48 +1,31 @@
 package Homework;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Exercise2 {
     public static void main(String[] args) {
-        String JohnAddressStranger = "милсдарь";
-        byte JohnAge = 50;
-        short JohnDotaHours = 32767;
-        int JohnIQ = 20;
-        long JohnMoney = 8000000;
-        double JohnBodyTemp = 36.6;
-        String JohnSadness = "Грустно, конечно это";
-        String JohnCurrency = "Зимбабвийские тугрики";
-        String JohnBye = "Счастливо";
-        String BillAddressStranger = "добряк";
-        byte BillAge = 21;
-        short BillDotaHours = 0;
-        int BillIQ = 120;
-        long BillMoney = 20000;
-        double BillBodyTemp = 37.3;
-        String BillBye = "Бывай";
-        String BillCurrency = "Доллары";
-        String BillSadness = "Прискорбно.....";
-        System.out.println("Здравствуй, " + JohnAddressStranger);
-        System.out.println("Здрав будь, " + BillAddressStranger);
-        System.out.println("Ты не знаешь меня, а я не знаю тебя, так что давай знакомиться. Меня звать Джон Большое Копыто");
-        System.out.println("Ну-с, тогда и я представлюсь, меня Билл зовут");
-        System.out.println("Формальности улажены, теперь перейдем к ключевым вопросам. Сколько у тебя часов в Доте?");
-        System.out.println(BillDotaHours + ". А у тебя сколько?");
-        System.out.println(JohnDotaHours + ". Следующий вопрос. Твой IQ, это очень важно");
-        System.out.println(BillIQ + ". Ну раз я назвал, то теперь твой черед");
-        System.out.println(JohnIQ + ".......");
-        System.out.println(BillSadness);
-        System.out.println(JohnSadness + ". Ну и ладно, теперь ты спрашивай.");
-        System.out.println("Ну не знаю, сколько у тебя там денег?");
-        System.out.println(JohnMoney);
-        System.out.println("ЧТООООООООООО? Это все " + BillCurrency + "?");
-        System.out.println("Я не верю в эти грязные зеленые бумажки, у меня " + JohnCurrency);
-        System.out.println("Ладно.... У меня вот " + BillMoney);
-        System.out.println(JohnCurrency + "?");
-        System.out.println(BillCurrency);
-        System.out.println("Глупец, ладно, мне скоро идти, давай свой последний вопрос");
-        System.out.println("Ну не знаю, у тебя есть сейчас температура?");
-        System.out.println("Нет, " + JohnBodyTemp + " А у тебя?");
-        System.out.println(BillBodyTemp);
-        System.out.println("Ладушки, " + JohnBye);
-        System.out.println(BillBye);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько тебе лет?");
+        long age = scanner.nextLong();
+        System.out.println("Как тебя зовут?");
+        String name = scanner.next();
+        System.out.println("Любимый вид спорта?");
+        String sport = scanner.next();
+        System.out.println("Чему равно число Пи?");
+        double Pi = scanner.nextDouble();
+        System.out.println("Назови свое любимое число от 1 до 10");
+        byte FavoriteNumber1to10 = scanner.nextByte();
+        System.out.println("А теперь назови от 1 до 100");
+        short FavoriteNumber1to100 = scanner.nextShort();
+        System.out.println("Сколько слов ты читаешь вслух за минуту?");
+        int Read = scanner.nextInt();
+        System.out.println("В каком году была Отечественная война?");
+        long Date = scanner.nextLong();
+        System.out.println("Кексики любишь?");
+        String Cake = scanner.next();
+        System.out.println("Смотрел винкс?");
+        String Winx = scanner.next();
     }
 }
+        
