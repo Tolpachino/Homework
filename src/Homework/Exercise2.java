12f67684c5ef3547254010961cfd50471d9f1050
Тимур <pklvp@mail.ru>
2023-09-19 19:30:06 +1000

    Test

---
+++ src/Homework/Exercise2.java
@@ -0,0 +1,48 @@
+package Homework;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        String JohnAddressStranger = "милсдарь";
+        byte JohnAge = 50;
+        short JohnDotaHours = 32767;
+        int JohnIQ = 20;
+        long JohnMoney = 8000000;
+        double JohnBodyTemp = 36.6;
+        String JohnSadness = "Грустно, конечно это";
+        String JohnCurrency = "Зимбабвийские тугрики";
+        String JohnBye = "Счастливо";
+        String BillAddressStranger = "добряк";
+        byte BillAge = 21;
+        short BillDotaHours = 0;
+        int BillIQ = 120;
+        long BillMoney = 20000;
+        double BillBodyTemp = 37.3;
+        String BillBye = "Бывай";
+        String BillCurrency = "Доллары";
+        String BillSadness = "Прискорбно.....";
+        System.out.println("Здравствуй, " + JohnAddressStranger);
+        System.out.println("Здрав будь, " + BillAddressStranger);
+        System.out.println("Ты не знаешь меня, а я не знаю тебя, так что давай знакомиться. Меня звать Джон Большое Копыто");
+        System.out.println("Ну-с, тогда и я представлюсь, меня Билл зовут");
+        System.out.println("Формальности улажены, теперь перейдем к ключевым вопросам. Сколько у тебя часов в Доте?");
+        System.out.println(BillDotaHours + ". А у тебя сколько?");
+        System.out.println(JohnDotaHours + ". Следующий вопрос. Твой IQ, это очень важно");
+        System.out.println(BillIQ + ". Ну раз я назвал, то теперь твой черед");
+        System.out.println(JohnIQ + ".......");
+        System.out.println(BillSadness);
+        System.out.println(JohnSadness + ". Ну и ладно, теперь ты спрашивай.");
+        System.out.println("Ну не знаю, сколько у тебя там денег?");
+        System.out.println(JohnMoney);
+        System.out.println("ЧТООООООООООО? Это все " + BillCurrency + "?");
+        System.out.println("Я не верю в эти грязные зеленые бумажки, у меня " + JohnCurrency);
+        System.out.println("Ладно.... У меня вот " + BillMoney);
+        System.out.println(JohnCurrency + "?");
+        System.out.println(BillCurrency);
+        System.out.println("Глупец, ладно, мне скоро идти, давай свой последний вопрос");
+        System.out.println("Ну не знаю, у тебя есть сейчас температура?");
+        System.out.println("Нет, " + JohnBodyTemp + " А у тебя?");
+        System.out.println(BillBodyTemp);
+        System.out.println("Ладушки, " + JohnBye);
+        System.out.println(BillBye);
+    }
+}
